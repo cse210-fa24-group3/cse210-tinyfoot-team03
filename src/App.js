da796c7f71e9bc9fb41b589d946c2edeb45e342f
Aryan Dokania <aryandokania2001@gmail.com>
2024-10-16 15:54:28 -0700

    dark mode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [theme, setTheme] = useState('light');
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
   return (
-    <div className="App">
+    <div className={`App ${theme}`}>
       <header className="App-header">
-        TinyFoot
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={toggleTheme} className="theme-toggle-btn">
+          {theme === 'light' ? 'Dark Mode' : 'Light Mode'}
+        </button>
       </header>
     </div>
   );
